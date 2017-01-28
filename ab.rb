ff404ebf3677d745e2ef1fcd1d72b9eb2904b654
VG <vaudoc@gmail.com>
2017-01-28 20:47:18 +0100

    change ab hash to sha256

--- ab.rb
+++ ab.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ab < Formula
   homepage 'http://httpd.apache.org/docs/trunk/programs/ab.html'
   url 'http://archive.apache.org/dist/httpd/httpd-2.4.9.tar.bz2'
-  md5 '5c9520f40e2852412cafc3c803e5d0b7'
+  sha256 '3e62175b6e4c756671e493a863cccfd0ffaf0ef86c7eeb6f7f8f486dd51602e1'
 
   depends_on 'pcre'
   depends_on 'libtool'
