66efa50337be3ded888b05fa2ed15104938bc65c
VG <vaudoc@gmail.com>
2018-07-13 23:54:00 +0200

    update sha256 for ab

--- ab.rb
+++ ab.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ab < Formula
   homepage 'http://httpd.apache.org/docs/trunk/programs/ab.html'
   url 'http://archive.apache.org/dist/httpd/httpd-2.4.9.tar.bz2'
-  sha256 '3e62175b6e4c756671e493a863cccfd0ffaf0ef86c7eeb6f7f8f486dd51602e1'
+  sha256 'f78cc90dfa47caf3d83ad18fd6b4e85f237777c1733fc9088594b70ce2847603'
 
   depends_on 'pcre'
   depends_on 'libtool'
